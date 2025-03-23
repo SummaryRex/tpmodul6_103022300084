da817e90f5c9527ed258ffccb7dbf51a19e9fded
SummaryRex <139709884+SummaryRex@users.noreply.github.com>
2025-03-24 05:09:15 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul6_103022300084;
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Design by Contract Fathian Alfiana");
+        
+        for (int i =0; i < 1000; i++)
+        {
+            video.IncreasePlayCount(6969);
+        }
+        video.PrintVideoDetails();
+    }
+}
